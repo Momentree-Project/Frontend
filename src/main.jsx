c4a22d8f6915a50f4c4b44dc7997822d5d09223b
ksuju <139477852+ksuju@users.noreply.github.com>
2025-04-29 15:47:51 +0900

    feat: vite 기본 페이지 및 스타일링 제거거

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import Schedule from './pages/schedule/index.jsx'
 
 createRoot(document.getElementById('root')).render(
