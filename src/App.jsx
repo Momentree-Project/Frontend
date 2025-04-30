f9dd98b498b44bfd5c7e39bdf7136fac711ac95e
ksuju <139477852+ksuju@users.noreply.github.com>
2025-04-30 14:58:01 +0900

    feat: 임시 홈 생성성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Schedule from './pages/schedule/index.jsx'
+import Home from './pages/home/index.jsx';
 
 function App() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/schedule" element={<Schedule />} />
             </Routes>
         </Router>
