6b0a375539c61a7345bb60b26c55d316297391e1
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-02 00:28:49 +0900

    feat: tailwind 관련 세팅팅

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Tailwind 플러그인 추가
-  ],
+  plugins: [react()],
 })
