4c3986b3835ffb7e594549fe07f9fdf75ffb2ab0
ksuju <139477852+ksuju@users.noreply.github.com>
2025-05-03 13:55:05 +0900

    feat: App.jsx 에 로그인페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Schedule from './pages/schedule/index.jsx'
 import Home from './pages/home/index.jsx';
+import Login from './pages/auth/index.jsx';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/schedule" element={<Schedule />} />
+                <Route path='/' element={<Login />} />
             </Routes>
         </Router>
     );
