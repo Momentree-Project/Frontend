384b0eda1cf5b4eb527b4514918f0a3c6ce8e3f6
eunoia73 <soo97703@naver.com>
2025-05-06 10:09:46 +0900

    feat: #6 로그인 이후 경로 지정

--- src/pages/auth/OAuthRedirectHandler.jsx
+++ src/pages/auth/OAuthRedirectHandler.jsx
@@ -19,6 +19,7 @@ const OAuth2RedirectHandler = () => {
             username: response.data.data.username,
             email: response.data.data.email,
             userCode: response.data.data.userCode,
+            birth: response.data.data.birth,
             coupleId: response.data.data.coupleId,
           };
           const accessToken = response.data.data.accessToken;
@@ -37,13 +38,19 @@ const OAuth2RedirectHandler = () => {
             axios.defaults.headers.common["Authorization"]
           );
 
-          // coupleId 유무에 따라 다른 페이지로 리다이렉트
-          if (loginUserInfo.coupleId) {
-            console.log("커플 연결 완료 상태: /home으로 리다이렉트");
-            navigate("/home", { replace: true });
-          } else {
-            console.log("커플 연결 필요 상태: /connect로 리다이렉트");
+          // coupleId와 birth 유무에 따라 다른 페이지로 리다이렉트
+          if (!loginUserInfo.birth) {
+            // birth 정보가 없으면 추가 정보 입력 페이지로 우선 이동
+            console.log("개인정보 입력 필요: /additional-info로 리다이렉트");
+            navigate("/additional-info", { replace: true });
+          } else if (!loginUserInfo.coupleId) {
+            // birth는 있지만 coupleId가 없으면 커플 연결 페이지로 이동
+            console.log("커플 연결 필요: /connect로 리다이렉트");
             navigate("/connect", { replace: true });
+          } else {
+            // birth와 coupleId 모두 있으면 홈으로 이동
+            console.log("모든 정보 완료 상태: /home으로 리다이렉트");
+            navigate("/home", { replace: true });
           }
         }
       } catch (error) {
